e909caf70bd7a646b9766d3625b32661ad23798e
Justin Weber <justin.weber@netl.doe.gov>
2017-04-19 23:36:58 -0400

    added more files

---
+++ batchphoto/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Batchphoto
+#
+# Copyright (c) 2017 Justin Weber
+# Licensed under the terms of the MIT License
+# see LICENSE
+
+version_info = (0, 0, 1, "dev0")
+
+__version__ = '.'.join(map(str, version_info))
+__license__ = 'Licensed under the terms of the MIT License'
+__project_url__ = 'https://github.com/onlyjus/batchphoto'
