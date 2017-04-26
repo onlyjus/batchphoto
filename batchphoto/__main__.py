e0cc0c35164bdc3e4d79ef083e08929d0a27dd38
Justin Weber <justin.weber@netl.doe.gov>
2017-04-26 07:21:36 -0400

    working on front end

---
+++ batchphoto/__main__.py
@@ -0,0 +1,14 @@
+
+
+# Import from the future for Python 2 and 3 compatability!
+from __future__ import print_function, absolute_import, unicode_literals
+
+import argparse
+
+import batchphoto
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
